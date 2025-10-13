8bdafdd5431563982a19f8de429f557f9c1d2890
yyrrcc <music_v@naver.com>
2025-10-13 11:52:18 +0900

    setting

---
+++ src/main/java/com/mycompany/sbbpjboard/BoardProjectSbbApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.sbbpjboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectSbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectSbbApplication.class, args);
+	}
+
+}
